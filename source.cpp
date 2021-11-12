5266a7be754e7a5cfbb14003a8e80573b808f31c
waymedev <haocaochien@icloud.com>
2021-11-12 14:56:57 +0700

    add source

---
+++ source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int tong(int a, int b);
+int hieu(int a, int b);
+
+int main()
+{
+	int a, b;
+	char phepToan;
+	cout << "Nhap so nguyen 1: ";
+	cin >> a;
+	cout << "Nhap so nguyen 2: ";
+	cin >> b;
+	cout << "Chon phep toan (+,-) ";
+	cin >> phepToan;
+	system("pause");
+	return 0;
+}
